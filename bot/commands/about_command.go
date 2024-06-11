6dc166545db77786f36ce9e867f14fd31b87f6a6
kkumar-gcc <krishkumar9352@gmail.com>
2024-06-11 14:49:58 +0530

    add about command

---
+++ bot/commands/about_command.go
@@ -0,0 +1,25 @@
+package commands
+
+import tele "gopkg.in/telebot.v3"
+
+type AboutCommand struct {
+}
+
+func (command *AboutCommand) Name() string {
+	return "/about"
+}
+
+func (command *AboutCommand) Description() string {
+	return "about the bot"
+}
+
+func (command *AboutCommand) Handle(ctx tele.Context) error {
+	err := ctx.Notify(tele.Typing)
+	if err != nil {
+		return err
+	}
+
+	message := ""
+
+	return ctx.Send(message, tele.ModeMarkdown)
+}
